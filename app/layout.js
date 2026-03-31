429c20da685c14cb0f19a88490ff667272382c37
OBGarage <the.ob.garage@gmail.com>
2026-03-31 11:50:37 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,11 @@ import "./globals.css";
 
 export const metadata = {
   title: "OB Mobile Garage",
-  description: "Mobile oil change service for 80831 and surrounding areas.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
     </html>
   );
